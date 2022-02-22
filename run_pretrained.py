39e3ee8f724c6c792ba43b1830925c20ea535b61
Felipe Martins <fbm2@cin.ufpe.br>
2022-02-22 14:27:32 -0300

    initial run_pretrained
    virtual display and val key when inactive

--- run_pretrained.py
+++ run_pretrained.py
@@ -16,24 +16,28 @@ from buffer import ReplayBuffer
 
 def info_to_log(info):
     return {
-        # 'manager/goal': info['manager_weighted_rw'][0],
-        # 'manager/ball_grad': info['manager_weighted_rw'][1],
+        'manager/goal': info['manager_weighted_rw'][0],
+        'manager/ball_grad': info['manager_weighted_rw'][1],
         # 'manager/move': info['manager_weighted_rw'][2],
         # 'manager/collision': info['manager_weighted_rw'][3],
         # 'manager/energy': info['manager_weighted_rw'][4],
-        'worker/dist': info['workers_weighted_rw'][0][0],
+        # 'worker/dist': info['workers_weighted_rw'][0][0],
         # 'worker/energy': info['workers_weighted_rw'][0][1],
     }
 
 
-def run_validation_ep(agent, env, target):
-    obs = env.reset()
+def run_validation_ep(m_agent, w_agent, env, opponent_policies):
+    m_obs = env.reset()
     done = False
     while not done:
-        w_obs = env.set_action_m(target)
-        action = agent.get_action(w_obs)
-        _obs, _, done, _ = env.step(np.array(action))
-        obs = _obs.manager
+        m_action = m_agent.policy_fn(m_agent.policy_params, m_obs)
+        w_obs = env.set_action_m(m_action)
+        w_action = w_agent.policy_fn(w_agent.policy_params, w_obs)
+        step_action = np.concatenate(
+            [w_action] + [[p()] for p in opponent_policies], axis=0
+        )
+        _obs, _, done, _ = env.step(step_action)
+        m_obs = _obs.manager
 
 
 def main(args):
@@ -81,33 +85,36 @@ def main(args):
     m_observation_space, m_action_space = env.get_spaces_m()
     w_observation_space, w_action_space = env.get_spaces_w()
 
-    agent = DDPG(w_observation_space, w_action_space, learning_rate, gamma, seed)
-    buffer = ReplayBuffer(w_observation_space, w_action_space, replay_capacity)
-
-    key, val_key = jax.random.split(key)
-    target = np.array(jax.random.uniform(key, m_action_space.shape))
+    m_agent = DDPG(m_observation_space, m_action_space, learning_rate, gamma, seed)
+    w_agent = DDPG(w_observation_space, w_action_space, learning_rate, gamma, seed)
+    w_agent.actor_params = checkpoints.restore_checkpoint(
+        './checkpoints/pretraining-worker', w_agent.actor_params
+    )
+    m_buffer = ReplayBuffer(m_observation_space, m_action_space, replay_capacity)
 
     m_obs = env.reset()
     rewards, ep_steps, done, q_losses, pi_losses = 0, 0, False, [], []
     for step in tqdm(range(total_training_steps), smoothing=0.01):
         if args.training_val_frequency and step % args.training_val_frequency == 0:
-            val_key, _ = jax.random.split(val_key)
-            val_target = np.array(jax.random.uniform(val_key, m_action_space.shape))
-            run_validation_ep(agent, val_env, val_target)
+            run_validation_ep(m_agent, w_agent, val_env, opponent_policies)
 
-        w_obs = env.set_action_m(target)[0]
-        action = np.array(agent.sample_action(w_obs))
-        _obs, reward, done, info = env.step(action)
-        terminal_state = False if not done or "TimeLimit.truncated" in info else True
-        buffer.add(
-            w_obs, action, 0.0, reward.workers[0], terminal_state, _obs.workers[0]
+        m_action = np.array(m_agent.sample_action(m_obs))
+        w_obs = env.set_action_m(m_action)
+        w_actions = np.array(w_agent.get_action(w_obs))
+        step_action = np.concatenate(
+            [w_actions] + [[p()] for p in opponent_policies], axis=0
         )
+        _obs, reward, done, info = env.step(step_action)
 
-        rewards += reward.workers[0]
+        terminal_state = False if not done or "TimeLimit.truncated" in info else True
+
+        m_buffer.add(m_obs, m_action, 0.0, reward.manager, terminal_state, _obs.manager)
+
+        rewards += reward.manager
         ep_steps += 1
         if step >= min_replay_size:
-            batch = buffer.get_batch(batch_size)
-            act_loss, crt_loss = agent.update(batch)
+            batch = m_buffer.get_batch(batch_size)
+            act_loss, crt_loss = m_agent.update(batch)
             pi_losses.append(act_loss)
             q_losses.append(crt_loss)
 
@@ -124,16 +131,14 @@ def main(args):
                 wandb.log(log, step=step)
                 q_losses, pi_losses = [], []
 
-        obs = _obs.manager
+        m_obs = _obs.manager
         if done:
-            obs = env.reset()
-            key, _ = jax.random.split(key)
-            target = np.array(jax.random.uniform(key, m_action_space.shape))
+            m_obs = env.reset()
             rewards, ep_steps = 0, 0
 
     checkpoints.save_checkpoint(
         f'./checkpoints/{args.wandb_name}',
-        agent.actor_params,
+        m_agent.actor_params,
         step=total_training_steps,
         overwrite=True,
     )
@@ -161,7 +166,7 @@ if __name__ == '__main__':
     parser.add_argument('--env-opponent-policy', type=str, default='off')
 
     # TRAINING
-    parser.add_argument('--training-total-steps', type=int, default=2500000)
+    parser.add_argument('--training-total-steps', type=int, default=5000000)
     parser.add_argument('--training-replay-min-size', type=int, default=100000)
     parser.add_argument('--training-batch-size', type=int, default=256)
     parser.add_argument('--training-gamma', type=float, default=0.95)
