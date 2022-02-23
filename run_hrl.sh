66f3d91b966e36081bd6e6a3f2e4a50428378b27
Felipe Martins <felipemartins96@gmail.com>
2022-02-23 14:24:34 -0300

    run hierarchical script

---
+++ run_hrl.sh
@@ -0,0 +1,4 @@
+python run_hrl.py --wandb-mode online --experiment-type hrl --wandb-name hrl-1v0
+python run_hrl.py --wandb-mode online --experiment-type hrl --wandb-name hrl-2v0 --env-n-robots-blue 2 
+python run_hrl.py --wandb-mode online --experiment-type hrl --wandb-name hrl-1v3 --env-n-robots-yellow 3
+python run_hrl.py --wandb-mode online --experiment-type hrl --wandb-name hrl-2v3 --env-n-robots-blue 2 --env-n-robots-yellow 3
