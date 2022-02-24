b064653eef30e919649bc087ed4f2f02271c27cd
Felipe Martins <fbm2@cin.ufpe.br>
2022-02-24 12:06:18 -0300

    update scripts for gonca rw

---
+++ run_base.sh
@@ -0,0 +1,4 @@
+python run_base.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name base-1v0
+python run_base.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name base-2v0 --env-n-robots-blue 2 
+python run_base.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name base-1v3 --env-n-robots-yellow 3
+python run_base.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name base-2v3 --env-n-robots-blue 2 --env-n-robots-yellow 3
--- run_pretrained.sh
+++ run_pretrained.sh
@@ -1,4 +1,4 @@
-# python run_pretrained.py --wandb-mode online --wandb-name pretrained-1v0
-python run_pretrained.py --wandb-mode online --wandb-name pretrained-2v0 --env-n-robots-blue 2 
-python run_pretrained.py --wandb-mode online --wandb-name pretrained-1v3 --env-n-robots-yellow 3
-python run_pretrained.py --wandb-mode online --wandb-name pretrained-2v3 --env-n-robots-blue 2 --env-n-robots-yellow 3
+python run_pretrained.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name pretrained-1v0
+python run_pretrained.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name pretrained-2v0 --env-n-robots-blue 2 
+python run_pretrained.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name pretrained-1v3 --env-n-robots-yellow 3
+python run_pretrained.py --env-name VSSHRL-v1 --wandb-mode online --wandb-name pretrained-2v3 --env-n-robots-blue 2 --env-n-robots-yellow 3
