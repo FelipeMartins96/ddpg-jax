44aaa5ec834dff11434c1b90e5b0d1d0b0d402cd
Felipe Martins <fbm2@cin.ufpe.br>
2022-03-03 11:29:59 -0300

    add sanity check experiments

--- run_base.sh
+++ run_base.sh
@@ -1,4 +1,4 @@
-python run_base.py --env-name VSSHRL-v1 --wandb-mode online --experiment-type base --wandb-name base-1v0
-python run_base.py --env-name VSSHRL-v1 --wandb-mode online --experiment-type base --wandb-name base-2v0 --env-n-robots-blue 2 
-python run_base.py --env-name VSSHRL-v1 --wandb-mode online --experiment-type base --wandb-name base-1v3 --env-n-robots-yellow 3
-python run_base.py --env-name VSSHRL-v1 --wandb-mode online --experiment-type base --wandb-name base-2v3 --env-n-robots-blue 2 --env-n-robots-yellow 3
+python run_base.py --env-name VSSHRL-v2 --wandb-mode online --wandb-project msc-week-9 --experiment-type base --wandb-name non-hrl-1v3 --env-n-robots-yellow 3
+python run_base.py --env-name VSSHRL-v2 --wandb-mode online --wandb-project msc-week-9 --experiment-type base --wandb-name non-hrl-1v2 --env-n-robots-yellow 2
+python run_base.py --env-name VSSHRL-v2 --wandb-mode online --wandb-project msc-week-9 --experiment-type base --wandb-name non-hrl-1v1 --env-n-robots-yellow 1
+python run_base.py --env-name VSSHRL-v2 --wandb-mode online --wandb-project msc-week-9 --experiment-type base --wandb-name non-hrl-1v0
