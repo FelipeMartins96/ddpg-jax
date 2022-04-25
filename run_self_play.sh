1b3807e27d1222dbea348fe919e62fed732cfff7
Felipe Martins <fbm2@cin.ufpe.br>
2022-04-25 11:33:00 -0300

    wip todo

--- run_self_play.sh
+++ run_self_play.sh
@@ -1,1 +1,8 @@
 python hrl_self.py --wandb-mode online --wandb-project vss-baguncinha-selfhrl --wandb-entity robocin --training-batch-size 64 
+
+# TODO
+# DEBUGAR VELOCIDADE
+# MUDAR PARA 1V1
+# RODAR MAIS DE UM EP DE VALIDAÇÃO, DIMINUIR A FREQUENCIA E NÃO GRAVAR TODOS OS EPS
+# SALVAR TUDO PARA REINICIAR (REPLAY BUFFER, OPTIMIZER, REDES)
+# AUMENTAR NOISE
