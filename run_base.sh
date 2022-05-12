b396be238eae2c0fa62dca7219e849cbd651458a
Felipe Martins <felipemartins96@gmail.com>
2022-05-11 23:25:59 -0300

    mean rewards experiments

--- run_base.sh
+++ run_base.sh
@@ -1,7 +1,4 @@
-python run_base.py  --env-n-robots-blue 3 --env-n-robots-yellow 3 --wandb-name correlated-3v3 --wandb-mode online --wandb-project msc-w19
-python run_base.py  --env-n-robots-blue 1 --env-n-robots-yellow 1 --wandb-name correlated-1v1 --wandb-mode online --wandb-project msc-w19
-python run_base.py  --env-n-robots-blue 2 --env-n-robots-yellow 1 --wandb-name correlated-2v1 --wandb-mode online --wandb-project msc-w19
-python run_base.py  --env-n-robots-blue 1 --env-n-robots-yellow 3 --wandb-name correlated-1v3 --wandb-mode online --wandb-project msc-w19
-python run_base.py  --env-n-robots-blue 2 --env-n-robots-yellow 3 --wandb-name correlated-2v3 --wandb-mode online --wandb-project msc-w19
-python run_base.py  --env-n-robots-blue 1 --env-n-robots-yellow 0 --wandb-name correlated-1v0 --wandb-mode online --wandb-project msc-w19
-python run_base.py  --env-n-robots-blue 2 --env-n-robots-yellow 0 --wandb-name correlated-2v0 --wandb-mode online --wandb-project msc-w19
+python run_base.py  --env-n-robots-blue 1 --env-n-robots-yellow 3 --wandb-name meanEnergyAndMove-1v3 --wandb-mode online --wandb-project msc-w19 & \
+python run_base.py  --env-n-robots-blue 2 --env-n-robots-yellow 3 --wandb-name meanEnergyAndMove-2v3 --wandb-mode online --wandb-project msc-w19 & \
+python run_base.py  --env-n-robots-blue 1 --env-n-robots-yellow 0 --wandb-name meanEnergyAndMove-1v0 --wandb-mode online --wandb-project msc-w19 & \
+python run_base.py  --env-n-robots-blue 2 --env-n-robots-yellow 0 --wandb-name meanEnergyAndMove-2v0 --wandb-mode online --wandb-project msc-w19
